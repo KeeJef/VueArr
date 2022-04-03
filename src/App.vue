d8aa4ddd3b21a3c01b9a3db39a9381f2d738a110
Kee Jefferys <kee@loki.network>
2022-04-03 13:39:47 +1000

    Table components

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="loadedTorrentBox">
+  <table id="torrentTable">
+    <tr>
+      <th>Category</th>
+      <th>Title</th>
+      <th>Uploaded Date</th>
+      <th>Link</th>
+      <th>Seeders</th>
+      <th>Leachers</th>
+    </tr>
+    <torrentItem />
+  </table>
+  </div>
+</template>
+
+<script>
+import torrentItem from "./components/torrentItem.vue";
+
+export default {
+  name: "App",
+  components: {
+    torrentItem,
+  },
+};
+</script>
+
+<style>
+.loadedTorrentBox {
+  padding-top: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+ #torrentTable {
+        width: 80%;
+        border-collapse: collapse;
+    }
+
+    #torrentTable td,
+    #torrentTable th {
+        border: 1px solid #ddd;
+        padding: 8px;
+    }
+
+    #torrentTable tr:nth-child(even) {
+        background-color: #f2f2f2;
+    }
+
+    #torrentTable tr:hover {
+        background-color: #ddd;
+    }
+
+    #torrentTable th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: left;
+        background-color: #005CC8;
+        color: white;
+    }
+</style>
---
+++ src/components/torrentItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <tr>
+    <td>Application</td>
+    <td>SUSE 10.1 BETA1 OSS DV .iso</td>
+    <td>02/02/2021</td>
+    <td>
+      <span class="magnetSpan"
+        ><a
+          href="magnet:?xt=urn:btih:186BEB2F74B525685E7EF7543F8696E7F968276A&amp;dn=SUSE%2010.1%20BETA1%20OSS%20DVD%20.iso&amp;tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&amp;tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&amp;tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&amp;tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&amp;tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&amp;tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&amp;tr=udp%3A%2F%2F47.ip-51-68-199.eu%3A6969%2Fannounce"
+          >🧲</a
+        ></span
+      >
+    </td>
+    <td>10</td>
+    <td>10</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+table {
+  border-collapse: collapse;
+  background-color: #f2f2f2;
+}
+
+body {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji",
+    "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+}
+
+</style>
